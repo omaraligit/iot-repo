cdbcff0b24d8d12d4fc30f6d37d9656771f72a89
Omar Ali <omarali0601520286@gmail.com>
2022-10-01 23:08:40 +0100

    servo demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
-#include <Arduino.h>
-
+#include <ESP32Servo.h>
+// create servo objects 
+Servo servo1;
 void setup()
 {
-
-  
+    servo1.attach(18);
 }
 
 void loop()
 {
-
+    servo1.write(0);
+    delay(2000);
+    servo1.write(45);
+    delay(2000);
+    servo1.write(90);
+    delay(2000);
+    servo1.write(135);
+    delay(2000);
+    servo1.write(170);
+    delay(2000);
 }
