d34d47313dfced0524f1904fd5a03961a6070ed8
Omar Ali <omarali0601520286@gmail.com>
2022-10-08 14:53:48 +0100

    gpio analog and digital

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 
 void setup()
 {
-
-  
+    Serial.begin(921600);
+    pinMode(23,INPUT);
+    pinMode(2,OUTPUT);
 }
 
 void loop()
 {
-
+    Serial.println(analogRead(GPIO_NUM_15));
+    analogWrite(2,analogRead(GPIO_NUM_15)*(3.3/4095));
+    delay(100);
 }
