7971e3ca10608e57542a449bbfb20d56d44af4ac
Omar Ali <omarali0601520286@gmail.com>
2022-10-08 02:10:18 +0100

    added adafruit libary and sample code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,44 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
 
 void setup()
 {
+    Serial.begin(921600);
+    Serial.println("OLED FeatherWing test");
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
 
-  
+    Serial.println("OLED begun");
+
+    // Show image buffer on the display hardware.
+    // Since the buffer is intialized with an Adafruit splashscreen
+    // internally, this will display the splashscreen.
+    display.display();
+    delay(1000);
+
+    // Clear the buffer.
+    display.clearDisplay();
+    display.display();
+
+    Serial.println(display.height());
+
+    // text display tests
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.print("Connecting to SSID\n'adafruit':");
+    display.print("connected!");
+    display.println("IP: 10.0.1.23");
+    display.println("Sending val #0");
+    display.setCursor(0, 0);
+    display.display(); // actually display all of the above
 }
 
 void loop()
 {
-
 }
